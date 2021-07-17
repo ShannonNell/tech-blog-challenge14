e6bab1eb819f53fad0bcb0377ae8fdbb64af9f74
Shannon <shannon.nell92@gmail.com>
2021-07-17 09:19:15 -0400

    Server connection and initial setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+sequelize.sync({force: false}).then ( () => {
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+});

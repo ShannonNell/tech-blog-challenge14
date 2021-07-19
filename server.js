16311fa04d0112c6bcfec71e1568a75dde8dc773
Shannon <shannon.nell92@gmail.com>
2021-07-19 14:27:18 -0400

    Simple homepage setup

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -5,9 +6,17 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// handlebars set up
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 //express
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use(routes);

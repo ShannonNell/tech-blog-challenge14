afc735abd4f3212a593fb3599b3e9e54951b5796
Shannon <shannon.nell92@gmail.com>
2021-07-19 19:16:18 -0400

    Rough homepage template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+      id: 1,
+      content: 'Hello. My name is Harry Potter.',
+      title: 'Who am I?',
+      created_at: new Date(),
+      comments: [{}, {}],
+      user: {
+          username: 'Harry Potter'
+      }
+  });
 });
 
 module.exports = router;

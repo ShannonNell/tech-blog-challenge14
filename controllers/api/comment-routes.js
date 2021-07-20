49e22f867d3e3f1607a126d21f885260da03c04d
Shannon <shannon.nell92@gmail.com>
2021-07-20 17:15:13 -0400

    Comment form funcitonal for all

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,16 +13,18 @@ router.get('/', (req, res) => {
 
 // POST/create new comment -> /api/comments
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.body.user_id,
+            post_id: req.body.post_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 // DELETE a comment -> /api/comments/:id
@@ -34,7 +36,7 @@ router.delete('/:id', (req, res) => {
     })
         .then(dbCommentData => {
             if (!dbCommentData) {
-                res.status(404).json({ message: 'No comment found with this id!'});
+                res.status(404).json({ message: 'No comment found with this id!' });
                 return;
             }
             res.json(dbCommentData);

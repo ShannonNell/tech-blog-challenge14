a5f8bf05307212fef43abd14468af6ef86efb9bd
Shannon <shannon.nell92@gmail.com>
2021-07-25 16:55:17 -0400

    Minor tweaks and testing

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET/read all users -> /api/users
 router.get('/', (req, res) => {
@@ -99,7 +100,7 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
@@ -110,7 +111,7 @@ router.post('/logout', (req, res) => {
 });
 
 // PUT/update single user -> /api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     User.update(req.body, {
         individualHooks: true,
         where: {
@@ -131,7 +132,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE single user -> /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
